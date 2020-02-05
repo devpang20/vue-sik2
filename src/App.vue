57b88f51f8af2d6b5c69f9bbd23d24d34ec72335
sik2 <devpang20@gmail.com>
2020-02-05 17:40:30 +0900

    vuex2

--- src/App.vue
+++ src/App.vue
@@ -33,13 +33,7 @@ export default {
       this.todoItems.splice(index, 1);
     }
   },
-  created() {
-		if (localStorage.length > 0) {
-			for (var i = 0; i < localStorage.length; i++) {
-				this.todoItems.push(localStorage.key(i));
-			}
-		}
-  },
+
   components: {
     'TodoHeader': TodoHeader,
     'TodoInput': TodoInput,
